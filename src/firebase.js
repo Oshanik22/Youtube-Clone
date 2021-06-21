7c15c943d0a715be55b6b1cafe3a9b0f0263de2c
Oshanik22 <oshanik22@gmail.com>
2021-06-21 12:05:41 +0530

    changed readme

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBppbG1cjVaDBTaPN8x3sr8nfeAk0_1cRw",
+    authDomain: "clone-51231.firebaseapp.com",
+    projectId: "clone-51231",
+    storageBucket: "clone-51231.appspot.com",
+    messagingSenderId: "92832410788",
+    appId: "1:92832410788:web:d7c3a3a3bc8e3e62410a31",
+    measurementId: "G-KDZQV17ZQD"
+  };
